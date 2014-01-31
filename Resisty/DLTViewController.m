d6604c5512a8f90bfb03a77ec9a652a1d6dc4dfe
Danielle Lancashire <dani@builds.terrible.systems>
2014-01-31 22:23:52 +0000

    things

--- Resisty/DLTViewController.m
+++ Resisty/DLTViewController.m
@@ -37,19 +37,26 @@
 	return 1;
 }
 
-- (NSInteger)tableView:(UITableView *)tableView
- numberOfRowsInSection:(NSInteger)section
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
 	return 0;
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView
-		 cellForRowAtIndexPath:(NSIndexPath *)indexPath
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
 	return [tableView dequeueReusableCellWithIdentifier:@"cell"];
 }
 
 #pragma mark - UITableViewDelegate Methods
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	// ToDo: TableViewRow Selection
+}
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	return YES;
+}
 
 @end
